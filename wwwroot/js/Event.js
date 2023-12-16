e79e1243791ebf42074e0fe3c86d001a63221f0e
Ahmed Essam Ahmed Hassan <ahmed.essam@student.giu-uni.de>
2023-12-16 14:42:43 +0200

    Events Updates

--- wwwroot/js/Event.js
+++ wwwroot/js/Event.js
@@ -5,3 +5,14 @@
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
 }
+function toggleContainer() {
+    var container = document.getElementById('myContainer');
+    if (container.classList.contains('show')) {
+        container.classList.remove('show');
+        container.classList.add('hide');
+    } else {
+        container.classList.remove('hide');
+        container.classList.add('show');
+    }
+}
+
